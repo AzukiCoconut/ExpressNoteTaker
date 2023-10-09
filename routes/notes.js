19c13b96a61e9bb34ac427f8597fd58809da5fe6
AzukiCoconut <136940320+AzukiCoconut@users.noreply.github.com>
2023-10-09 15:22:16 -0300

    Add get, post and delete functions to Notes.js. Tested functionality

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,46 @@
 const note = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
+
+note.get("/", (req, res) => 
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+note.post("/", (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+
+        const newNote = {
+            title, 
+            text, 
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success', 
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in posting note');
+    }
+});
+
+note.delete('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.id !== noteId);
+
+            writeToFile('./db/db.json', result);
+
+            res.json(`Note ${noteId} has been deleted`);
+        });
+} );
+
+module.exports = note;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
+const api = require('./routes/index');
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,17 +9,13 @@ const app = express();
 //Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use('/api', api);
 
-app.use(express.static('public'));
-
-//GET Route for homepage
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-
 //Get Route for Notes page
-app.get('/notes', (res,req) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 //Get Route to direct users to the index page
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/pubic/index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT, () => console.log('App listening'));
