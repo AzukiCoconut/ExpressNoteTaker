a8e764fdaaae24afc2fe12f11f036f7952af57e2
AzukiCoconut <136940320+AzukiCoconut@users.noreply.github.com>
2023-10-09 15:35:07 -0300

    Added documentation to the code

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 const util = require('util');
 
+// Code is copied from the fsUtil.js used in the UNB Coding Bootcamp week 11 mini project
+
+// Read from file 
 const readFromFile = util.promisify(fs.readFile);
 
+// Write to file
 const writeToFile = (destination, content) => 
     fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => 
         err ? console.error(err) : console.info(`\nData written to ${destination}`)
 );
 
+// Read and then append to file
 const readAndAppend = (content, file) => {
     fs.readFile(file, 'utf8', (err, data) => {
         if (err) {
